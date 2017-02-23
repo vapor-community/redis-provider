deb8983fec45f668d69a0e2752f650b9b519703f
Tanner Nelson <me@tanner.xyz>
2017-02-23 16:11:03 +0100

    😤

--- Package.swift
+++ Package.swift
@@ -7,6 +7,6 @@ let package = Package(
         .Package(url: "https://github.com/vapor/redbird.git", majorVersion: 1),
 
         // Importing Vapor for access to the Provider protocol.
-        .Package(url: "https://github.com/vapor/vapor.git", Version(0,0,0))
+        .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"]))
     ]
 )
